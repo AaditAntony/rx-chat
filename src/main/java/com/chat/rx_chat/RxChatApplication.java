26ad14c3577f22a50f4a4001576d67ee2dd39a6f
Aadit Antony <aaditantony1112@gmail.com>
2025-11-15 16:32:42 +0530

    first commit

---
+++ src/main/java/com/chat/rx_chat/RxChatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.rx_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/rx_chat/RxChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.rx_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
